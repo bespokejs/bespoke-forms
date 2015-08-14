850147f7266f7ad087c9f4b8d808d406f96eb5ab
Dan Allen <dan.j.allen@gmail.com>
2015-08-13 19:59:12 -0600

    detect active form element more accurately

--- dist/bespoke-forms.js
+++ dist/bespoke-forms.js
@@ -1,7 +1,7 @@
 /*!
  * bespoke-forms v1.0.0
  *
- * Copyright 2014, Mark Dalgleish
+ * Copyright 2015, Mark Dalgleish
  * This content is released under the MIT license
  * http://mit-license.org/markdalgleish
  */
@@ -9,12 +9,14 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self);var f=o;f=f.bespoke||(f.bespoke={}),f=f.plugins||(f.plugins={}),f.forms=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 module.exports = function() {
   return function(deck) {
-    deck.slides.forEach(function(slide) {
-      slide.addEventListener('keydown', function(e) {
-        if (/INPUT|TEXTAREA|SELECT/.test(e.target.nodeName) || e.target.contentEditable === 'true') {
-          e.stopPropagation();
-        }
-      });
+    var acceptsInput = function(element) {
+      return ('disabled' in element) || element.contentEditable === 'true';
+    };
+
+    deck.parent.addEventListener('keydown', function(e) {
+      if (acceptsInput(e.target)) {
+        e.stopPropagation();
+      }
     });
   };
 };
